dc9337036a2a2d10b28c4f34fdc8866a8ca8224f
dubuqingfeng <1135326346@qq.com>
2016-04-21 17:45:03 +0800

    injection test

--- js/backend.js
+++ js/backend.js
@@ -33,7 +33,47 @@ chrome.extension.onMessage.addListener(function(request, _, sendResponse){
 });
 
 function beforeRequest(details) {
-  if (localStorage['status'] == 'start') {
-    alert(details.url)
-  }
+	if (localStorage['status'] == 'start') {
+		if(confirm("确定要添加扫描？"+ details.url))
+		{
+			var sqlmapIpPort="http://127.0.0.1:8775";
+			var payload = {'url': details.url}
+
+			console.log(payload);
+			Connection('GET',sqlmapIpPort+'/task/new','',function(callback){
+	            var response=JSON.parse(callback);
+	            if(response.success){
+	                Connection('POST',sqlmapIpPort+'/scan/'+response.taskid+'/start',JSON.stringify(payload),function(callback){
+	                        var responseTemp=JSON.parse(callback);
+	                        if(!responseTemp.success){
+	                            alert('url send to sqlmapapi error');
+	                        }
+	                    }
+	                )
+	            }
+	            else{
+	                alert('sqlmapapi create task error');
+	            }
+	        }
+		    )
+		}
+	}
 }
+
+function Connection(Sendtype,url,content,callback){ 
+    if (window.XMLHttpRequest){ 
+        var xmlhttp=new XMLHttpRequest(); 
+    } 
+    else{ 
+        var xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); 
+    } 
+    xmlhttp.onreadystatechange=function(){ 
+        if(xmlhttp.readyState==4&&xmlhttp.status==200) 
+        { 
+            callback(xmlhttp.responseText); 
+        } 
+    } 
+    xmlhttp.open(Sendtype,url,true); 
+    xmlhttp.setRequestHeader("Content-Type","application/json"); 
+    xmlhttp.send(content); 
+} 
--- js/popup.js
+++ js/popup.js
@@ -2,27 +2,45 @@ $('#startorpause').click(function(){
 	// 获取状态
 	if (localStorage['status'] == 'start') {
 		Settings.sendMessageBack('pause', {}, function(response){
-			console.log("test");
-			window.close();
+			closePopup();
 	    });
 		document.getElementById('startorpause').innerHTML = chrome.i18n.getMessage("startscan");
 	} else {
 		Settings.sendMessageBack('start', {}, function(response){
-			console.log("test");
-			window.close();
+			closePopup();
 	    });
 		document.getElementById('startorpause').innerHTML = chrome.i18n.getMessage("pausescan");
 	}
-	
 });
 $('#result-list').click(function(){
-
+	openOrFocusPage('result.html');
+    closePopup();
 });
 $('#options').click(function(){
-	var a=chrome.extension.getBackgroundPage();
-	window.close();
-	a.OpenExtensionUrl("../options.html");
+	openOrFocusPage('options.html');
+    closePopup();
 });
+function closePopup() {
+	window.close();
+}
+
+function openOrFocusPage(url) {
+   var optionsUrl = chrome.extension.getURL(url); 
+   chrome.tabs.query({}, function(extensionTabs) {
+      var found = false;
+      for (var i=0; i < extensionTabs.length; i++) {
+         if (optionsUrl == extensionTabs[i].url) {
+            found = true;
+            console.log("tab id: " + extensionTabs[i].id);
+            chrome.tabs.update(extensionTabs[i].id, {"selected": true});
+         }
+      }
+      if (found == false) {
+          chrome.tabs.create({url: url});
+      }
+   });
+}
+
 $(document).ready(function() {
 	if (localStorage['status'] == 'start') {
 		document.getElementById('startorpause').innerHTML = chrome.i18n.getMessage("pausescan");
